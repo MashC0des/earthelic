2a6013f6233d788e84357087e364d3ee0191f744
MashCodes <patelmeet5977@gmail.com>
2024-12-11 14:18:16 +0530

    changes

---
+++ script.js
@@ -0,0 +1,13 @@
+function showreg(){
+    const show = document.querySelector('.wrapper1')
+    show.style.display = 'block'
+    const hide = document.querySelector('.wrapper')
+    hide.style.display = 'none'
+}
+
+function showlog(){
+    const show = document.querySelector('.wrapper')
+    show.style.display = 'block'
+    const hide = document.querySelector('.wrapper1')
+    hide.style.display = 'none'
+}
