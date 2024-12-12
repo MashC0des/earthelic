fff9300ac03a8bf671237b24a7ba1e9744d2284e
MashCodes <patelmeet5977@gmail.com>
2024-12-12 15:48:28 +0530

    major changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// --------------------login/reg-----------------
 function showreg(){
     const show = document.querySelector('.wrapper1')
     show.style.display = 'block'
@@ -10,4 +11,33 @@ function showlog(){
     show.style.display = 'block'
     const hide = document.querySelector('.wrapper1')
     hide.style.display = 'none'
+}
+
+// -----------------pagination--------------------
+let link = document.getElementsByClassName("link")
+let currentValue = 1;
+function activeLink(){
+    for(l of link){
+        l.classList.remove("active");
+    }
+    event.target.classList.add("active");
+    currentValue = event.target.value;
+}
+function backbtn(){
+    if(currentValue>1){
+        for(l of link){
+            l.classList.remove("active");
+        }
+        currentValue--;
+        link[currentValue-1].classList.add("active");
+    }
+}
+function nextbtn(){
+    if(currentValue<8){
+        for(l of link){
+            l.classList.remove("active");
+        }
+        currentValue++;
+        link[currentValue-1].classList.add("active");
+    }
 }
